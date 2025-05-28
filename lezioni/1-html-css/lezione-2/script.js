da1ddf16229df494c922666ef0b038bd6da7a23c
Ruma <matteoh8@hotmail.it>
2025-05-28 17:55:14 +0200

    ex1

---
+++ lezioni/1-html-css/lezione-2/script.js
@@ -0,0 +1,8 @@
+// Esempio di script JavaScript
+document.querySelector('button[type="button"]').addEventListener('click', function () {
+  alert('Dati salvati!');
+});
+
+document.querySelector('button[type="reset"]').addEventListener('click', function () {
+  alert('Form resettato!');
+});

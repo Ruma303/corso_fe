77085c67911c65b7fd74c2f29a054d7adeba1954
Ruma <matteoh8@hotmail.it>
2025-06-05 17:58:02 +0200

    json-server

--- lezioni/2-javascript/ajax.js
+++ lezioni/2-javascript/ajax.js
@@ -1,3 +1,5 @@
+// Richiesta con fetch al server Express
+
 fetch("http://localhost:8000/api/user")
   .then((response) => {
     if (!response.ok) {
@@ -7,3 +9,22 @@ fetch("http://localhost:8000/api/user")
   })
   .then((data) => console.log("Risorsa trovata: ", data))
   .catch((error) => console.error("Errore nell'elaborazione: ", error));
+
+
+// Richiesta con JSON-server:
+// usare npx json-server --watch db/db.json
+// con il terminale da questa cartella
+
+fetch("http://localhost:3000/1", {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("Risorsa trovata: ", data);
+  })
+  .catch((error) => {
+    console.error("Errore nell'elaborazione: ", error);
+  })
